c717b90fbe00991c3b3380283443caf41d320b5c
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-02-06 01:32:35 +0900

    Impl: ex03
    課題ex03実装完了

---
+++ ex03/capital_city.py
@@ -0,0 +1,35 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    capital_city.py                                    :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/02/06 00:59:53 by kefujiwa          #+#    #+#              #
+#    Updated: 2021/02/06 01:31:50 by kefujiwa         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import sys
+
+states = {
+"Oregon" : "OR",
+"Alabama" : "AL",
+"New Jersey": "NJ",
+"Colorado" : "CO"
+}
+
+capital_cities = {
+"OR": "Salem",
+"AL": "Montgomery",
+"NJ": "Trenton",
+"CO": "Denver"
+}
+
+if len(sys.argv) == 2:
+	try:
+		print(capital_cities[states[sys.argv[1]]])
+	except KeyError:
+		print("Unknown state.")
+else:
+	print("Invalid arguments.")
