b4499d4410cdf92a930f87b74aee692c865a7346
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-02-06 00:11:01 +0900

    Impl: ex00
    課題ex00の実装完了

---
+++ ex00/var.py
@@ -0,0 +1,31 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    var.py                                             :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/02/04 16:20:26 by kefujiwa          #+#    #+#              #
+#    Updated: 2021/02/06 00:02:44 by kefujiwa         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+v_int = 42
+v_str_n = "42"
+v_str_s = "quarante-deux"
+v_float = 42.0
+v_bool = True
+v_list = [42]
+v_dict = {42: 42}
+v_tuple = (42,)
+v_set = set()
+
+print(f"{v_int} is type of {type(v_int)}")
+print(f"{v_str_n} is type of {type(v_str_n)}")
+print(f"{v_str_s} is type of {type(v_str_s)}")
+print(f"{v_float} is type of {type(v_float)}")
+print(f"{v_bool} is type of {type(v_bool)}")
+print(f"{v_list} is type of {type(v_list)}")
+print(f"{v_dict} is type of {type(v_dict)}")
+print(f"{v_tuple} is type of {type(v_tuple)}")
+print(f"{v_set} is type of {type(v_set)}")
