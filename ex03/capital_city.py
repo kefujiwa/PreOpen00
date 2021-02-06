68dda4dbc765a91b34b2539e64b45bd1d310cd54
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-02-06 22:11:02 +0900

    Fix: change tab to space
    タブをスペースに変更

--- ex03/capital_city.py
+++ ex03/capital_city.py
@@ -6,28 +6,27 @@
 #    By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/02/06 00:59:53 by kefujiwa          #+#    #+#              #
-#    Updated: 2021/02/06 02:00:19 by kefujiwa         ###   ########.fr        #
+#    Updated: 2021/02/06 22:12:50 by kefujiwa         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
 
 states = {
-"Oregon" : "OR",
-"Alabama" : "AL",
-"New Jersey": "NJ",
-"Colorado" : "CO"
+    "Oregon" : "OR",
+    "Alabama" : "AL",
+    "New Jersey": "NJ",
+    "Colorado" : "CO"
 }
-
 capital_cities = {
-"OR": "Salem",
-"AL": "Montgomery",
-"NJ": "Trenton",
-"CO": "Denver"
+    "OR": "Salem",
+    "AL": "Montgomery",
+    "NJ": "Trenton",
+    "CO": "Denver"
 }
 
 if len(sys.argv) == 2:
-	try:
-		print(capital_cities[states[sys.argv[1]]])
-	except KeyError:
-		print("Unknown state")
+    try:
+        print(capital_cities[states[sys.argv[1]]])
+    except KeyError:
+        print("Unknown state")
