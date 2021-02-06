1efc9350a628d4433d6446c547cf1f0187d52a6f
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-02-06 21:19:47 +0900

    Fix: add error handling & parameter of open function & change method of reading file_object
    エラーハンドリング追加。ファイル読み込み方法変更。

--- ex01/numbers.py
+++ ex01/numbers.py
@@ -6,15 +6,15 @@
 #    By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/02/06 00:12:52 by kefujiwa          #+#    #+#              #
-#    Updated: 2021/02/06 00:40:25 by kefujiwa         ###   ########.fr        #
+#    Updated: 2021/02/06 21:18:45 by kefujiwa         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 filename = "numbers.txt"
 
-with open(filename) as file_object:
-	for line in file_object:
-		if ',' in line:
-			print(line[:-2])
-		else:
-			print(line)
+try:
+    with open(filename, "r", encoding="UTF-8") as file_object:
+        content = file_object.read()
+        print(content.replace(",", ""))
+except OSError as e:
+    print(e)
