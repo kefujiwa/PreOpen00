d76aa203871417d2c05ddb8f0a68f04ecfb6e00b
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-02-07 09:53:51 +0900

    Fix: add sep variable
    is type of を文字列としてsep変数に代入し、これを利用して標準出力

--- ex00/var.py
+++ ex00/var.py
@@ -6,7 +6,7 @@
 #    By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/02/04 16:20:26 by kefujiwa          #+#    #+#              #
-#    Updated: 2021/02/06 00:02:44 by kefujiwa         ###   ########.fr        #
+#    Updated: 2021/02/07 09:53:09 by kefujiwa         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,13 +19,14 @@ v_list = [42]
 v_dict = {42: 42}
 v_tuple = (42,)
 v_set = set()
+sep = "is type of"
 
-print(f"{v_int} is type of {type(v_int)}")
-print(f"{v_str_n} is type of {type(v_str_n)}")
-print(f"{v_str_s} is type of {type(v_str_s)}")
-print(f"{v_float} is type of {type(v_float)}")
-print(f"{v_bool} is type of {type(v_bool)}")
-print(f"{v_list} is type of {type(v_list)}")
-print(f"{v_dict} is type of {type(v_dict)}")
-print(f"{v_tuple} is type of {type(v_tuple)}")
-print(f"{v_set} is type of {type(v_set)}")
+print(v_int, sep, type(v_int))
+print(v_str_n, sep, type(v_str_n))
+print(v_str_s, sep, type(v_str_s))
+print(v_float, sep, type(v_float))
+print(v_bool, sep, type(v_bool))
+print(v_list, sep, type(v_list))
+print(v_dict, sep, type(v_dict))
+print(v_tuple, sep, type(v_tuple))
+print(v_set, sep, type(v_set))
