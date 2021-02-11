f7d3962dd52d7b50d35b94c455dcdf903601cf1a
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-02-11 21:32:59 +0900

    refactor: define my_var function and put all the process in it

--- ex00/var.py
+++ ex00/var.py
@@ -6,7 +6,7 @@
 #    By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/02/04 16:20:26 by kefujiwa          #+#    #+#              #
-#    Updated: 2021/02/07 09:53:09 by kefujiwa         ###   ########.fr        #
+#    Updated: 2021/02/11 21:32:00 by kefujiwa         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,12 +21,15 @@ v_tuple = (42,)
 v_set = set()
 sep = "is type of"
 
-print(v_int, sep, type(v_int))
-print(v_str_n, sep, type(v_str_n))
-print(v_str_s, sep, type(v_str_s))
-print(v_float, sep, type(v_float))
-print(v_bool, sep, type(v_bool))
-print(v_list, sep, type(v_list))
-print(v_dict, sep, type(v_dict))
-print(v_tuple, sep, type(v_tuple))
-print(v_set, sep, type(v_set))
+def my_var():
+    print(v_int, sep, type(v_int))
+    print(v_str_n, sep, type(v_str_n))
+    print(v_str_s, sep, type(v_str_s))
+    print(v_float, sep, type(v_float))
+    print(v_bool, sep, type(v_bool))
+    print(v_list, sep, type(v_list))
+    print(v_dict, sep, type(v_dict))
+    print(v_tuple, sep, type(v_tuple))
+    print(v_set, sep, type(v_set))
+
+my_var()
